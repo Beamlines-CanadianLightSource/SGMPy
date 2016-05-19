35ed45563751e731c4c8cc4d260cc32b3bf445da
Rongli H <roh919@mail.usask.ca>
2016-05-19 16:53:32 -0600

    Move the location of 3 Python Scripts
    Move basic_plot, average_plot and summary_plot into scripts package

--- scripts/summary_plot.py
+++ scripts/summary_plot.py
@@ -48,4 +48,5 @@ def generateGoodScanArray(scanNumArray,badScanStr):
 		scanNumArray.remove(badScanNumArray[i])
 
 	print "These are all good scan numbers: ", scanNumArray
-	return scanNumArray
+	goodScan = scanNumArray
+	return goodScan
