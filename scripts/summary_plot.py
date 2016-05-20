35d8f3e729a4f3562feccb92ba7ca518189664b3
Rongli H <roh919@mail.usask.ca>
2016-05-20 16:09:44 -0600

    Rename and update 3 example files

--- scripts/summary_plot.py
+++ scripts/summary_plot.py
@@ -15,9 +15,11 @@ def generateSummaryPlot(fileDirectory):
 		# scan number start from 1
 		scanNumList.fill(scanNum+1)
 		print "Generating plot for scan No.", scanNum+1
-		plt.scatter(sgmData[0][scanNum]['Energy'], scanNumList, c=sgmData[0][scanNum]['TEY'],  s=5, linewidths=0)
+		plt.scatter(sgmData[0][scanNum]['Energy'], scanNumList, c=sgmData[0][scanNum]['TEY'],  s=10, linewidths=0)
 		print "Generated plot for scan No.", scanNum+1, "completed"
     
+	plt.ylim(0, totalScanNumber+1)
+
 	# add lable for x and y axis
 	plt.xlabel('Incident Energy (ev)')
 	plt.ylabel('Scan Numbers')
