394b09115680a349bcbbf714167f391ced213839
Rongli H <roh919@mail.usask.ca>
2016-06-14 14:37:05 -0600

    Add functions to read and plot data from hdf5 files
    Also refactor and rename functions in average_plot.py

--- scripts/average_plot.py
+++ scripts/average_plot.py
@@ -1,4 +1,4 @@
-# Selection and Binning
+# Selection and binning
 
 from praxes.io import spec
 import matplotlib
@@ -9,60 +9,58 @@ from scan_details import *
 from basic_plot import *
 
 
-def prepare_average_plot(good_scan, opened_file, start_energy, end_energy, number_of_bins, start_range_of_interest, end_range_of_interest):
+def prepare_bin_plot(good_scan, opened_file, start_energy, end_energy, number_of_bins, start_range_of_interest, end_range_of_interest):
     
-	data_point_array = get_good_datapoint(good_scan, opened_file)
+	energy_array, mca_array, scaler_array = get_good_datapoint(good_scan, opened_file)
 
 	temp_array = create_bins(start_energy, end_energy, number_of_bins)
 	edges_array = temp_array[0]
 	bins_mean_array = temp_array[1]
-	assigned_data_array = assign_data(data_point_array , edges_array)
+	assigned_data_array = assign_data(energy_array , edges_array)
     # calculate average
-	result = calculate_avg_mca(assigned_data_array, data_point_array)
-	avg_mca = result[:-1]
+	result = calculate_bin_mca(assigned_data_array, mca_array)
+	bin_mca = result[:-1]
 	empty_bin_num = result[-1]
-	avg_scaler = calculate_avg_scalers(assigned_data_array, data_point_array)
-	pyf_data = get_pfy_avg(avg_mca, start_range_of_interest, end_range_of_interest)
+	bin_scaler = calculate_bin_scalers(assigned_data_array, scaler_array)
+	pyf_data = get_pfy_bin(bin_mca, start_range_of_interest, end_range_of_interest)
     
     #remove empty bins
     
-	return bins_mean_array[:-empty_bin_num], avg_mca, avg_scaler, pyf_data
+	return bins_mean_array[:-empty_bin_num], bin_mca, bin_scaler, pyf_data
 
 
 # Eliminate bad scans and select good scans (data points)
 def get_good_datapoint(good_scan_Array, opened_file):
     
 	print "Total good scan numbers:", len(good_scan_Array)
-	# initial arrays    
-	scan=[]
-	mca1=[]
-	mca2=[]
-	mca3=[]
-	mca4=[]
     
+	energy_array = []
+	scaler_array = [[[],[],[]] for i in range(len(good_scan_Array))]
+	mca_array=[[[],[],[],[]] for i in range(len(good_scan_Array))]
+
 	# open and read all data from the file and it could take a while
 	scan, mca1, mca2, mca3, mca4 = openAllSGMXAS(opened_file)
-    
-	# Initial arrayOfPoints
-	data_array=[[[],[],[],[],[]] for i in range(len(good_scan_Array))]
-    
+        
 	for i in range (0, len(good_scan_Array)):
 		# scan number is start from 1
 		# print "This is the scan number: ", goodScanArray[i]
 		# array index is start from 0
 		# get all scalers of good scans from original scans' array
-		data_array[i][0] = scan[good_scan_Array[i]-1]
+		energy_array.append(scan[good_scan_Array[i]-1]['Energy'])
+		scaler_array[i][0] = scan[good_scan_Array[i]-1]['TEY']
+		scaler_array[i][1] = scan[good_scan_Array[i]-1]['I0']
+		scaler_array[i][2] = scan[good_scan_Array[i]-1]['Diode']
 		# get all MCA1 of good scans from original scans
-		data_array[i][1] = mca1[good_scan_Array[i]-1]
+		mca_array[i][0] = mca1[good_scan_Array[i]-1]
 		# get all MCA2 of good scans from original scans
-		data_array[i][2] = mca2[good_scan_Array[i]-1]
+		mca_array[i][1] = mca2[good_scan_Array[i]-1]
 		# get all MCA3 of good scans from original scans
-		data_array[i][3] = mca3[good_scan_Array[i]-1]
+		mca_array[i][2] = mca3[good_scan_Array[i]-1]
 		# get all MCA4 of good scans from original scans
-		data_array[i][4] = mca4[good_scan_Array[i]-1]
-	return data_array
+		mca_array[i][3] = mca4[good_scan_Array[i]-1]
+	return energy_array, mca_array, scaler_array
 
-# create bins (for testing startEnergy = 690, endEnergy = 750, numberOfBins = 600, energyArray = scan[goodScan_2[i]-1]['Energy'])
+# create bins (startEnergy = 690, endEnergy = 750, numberOfBins = 600)
 def create_bins(start_energy, end_energy, num_of_bins):
 	print "Start creating bins" 
 	num_of_edges = num_of_bins + 1
@@ -83,7 +81,7 @@ def create_bins(start_energy, end_energy, num_of_bins):
 	return  edges_array, mean_energy_array
 
 
-def assign_data (data_array, edges):
+def assign_data (energy_array, edges):
 	bin_num = len(edges) - 1
 	bin_array = [[] for i in range(bin_num)]
 
@@ -91,136 +89,136 @@ def assign_data (data_array, edges):
 	bin_width = (edges[-1] - edges[0]) / bin_num
 	print "The width of a bin is:", bin_width
 	print "Start assigning data points into bins" 
-	for scan_index in range (0, len(data_array)):
-		for datapoint_index in range (0, len(data_array[scan_index][0]['Energy']) ):
-			x = data_array[scan_index][0]['Energy'][datapoint_index] - edges[0]
-			# how to get integer part + 1?????
+	for scan_index in range (0, len(energy_array)):
+		for datapoint_index in range (0, len(energy_array[scan_index]) ):
+			x = energy_array[scan_index][datapoint_index] - edges[0]
+			#Get integer part and plus 1
 			assign_bin_num = int(x / bin_width) + 1
-			# print assignBinNum
+			# print assign_bin_num
 			bin_array[assign_bin_num-1].append([scan_index, datapoint_index])
 	print "Assign data points completed"   
 	return bin_array
 
-def calculate_avg_mca(arrayOfBins, arrayOfPoints):
-	binNum = len(arrayOfBins)
+def calculate_bin_mca(bin_array, mca_array):
+	bin_num = len(bin_array)
 	empty_bins = 0
-	# Initial 4 arrays for 4 Average of MCAs
+	# Initial 4 arrays for 4 average of MCAs
 	# Added 256 of zero into each sub array, so that it could calculate summary and then get the average
-	mca1AvgArray = np.zeros(shape=(binNum,256))
-	mca2AvgArray = np.zeros(shape=(binNum,256))
-	mca3AvgArray = np.zeros(shape=(binNum,256))
-	mca4AvgArray = np.zeros(shape=(binNum,256))
+	mca1_bin_array = np.zeros(shape=(bin_num,256))
+	mca2_bin_array = np.zeros(shape=(bin_num,256))
+	mca3_bin_array = np.zeros(shape=(bin_num,256))
+	mca4_bin_array = np.zeros(shape=(bin_num,256))
 
-	print "Start calcualting Average of SDD1(MCA1), SDD2(MCA2), SDD3(MCA3) & SDD4(MCA4)..."
+	print "Start calcualting average of SDD1(MCA1), SDD2(MCA2), SDD3(MCA3) & SDD4(MCA4)..."
         
-	for index1 in range (0, binNum):
+	for index1 in range (0, bin_num):
 		# get the total number of data points in a particular bins
-		totalDataPoints = len(arrayOfBins[index1])
+		total_data_point = len(bin_array[index1])
 
-		for index2 in range (0, totalDataPoints):
+		for index2 in range (0, total_data_point):
 			# get index of scans
-			indexOfScan = arrayOfBins[index1][index2][0]
+			index_of_scan = bin_array[index1][index2][0]
 			# get index of data points
-			indexOfDataPoint = arrayOfBins[index1][index2][1]
-			# print "indexOfScan: ", indexOfScan_2, "  ;  ", "indexOfDataPoint: ", indexOfDataPoint
+			index_of_data_point = bin_array[index1][index2][1]
+			# print "index_of_scan: ", index_of_scan_2, "  ;  ", "index_of_data_point: ", index_of_data_point
 
 			# calculate the sum of MCA1
-			mca1AvgArray[index1] = mca1AvgArray[index1] + arrayOfPoints[indexOfScan][1][indexOfDataPoint]
+			mca1_bin_array[index1] = mca1_bin_array[index1] + mca_array[index_of_scan][0][index_of_data_point]
 			# calculate the sum of MCA2
-			mca2AvgArray[index1] = mca2AvgArray[index1] + arrayOfPoints[indexOfScan][2][indexOfDataPoint]
+			mca2_bin_array[index1] = mca2_bin_array[index1] + mca_array[index_of_scan][1][index_of_data_point]
 			# calculate the sum of MCA3
-			mca3AvgArray[index1] = mca3AvgArray[index1] + arrayOfPoints[indexOfScan][3][indexOfDataPoint]
+			mca3_bin_array[index1] = mca3_bin_array[index1] + mca_array[index_of_scan][2][index_of_data_point]
 			# calculate the sum of MCA4
-			mca4AvgArray[index1] = mca4AvgArray[index1] + arrayOfPoints[indexOfScan][4][indexOfDataPoint]
+			mca4_bin_array[index1] = mca4_bin_array[index1] + mca_array[index_of_scan][3][index_of_data_point]
             
-		# print "Bin No.", index1+1, "; it contains ", totalDataPoints, "data points"
+		# print "Bin No.", index1+1, "; it contains ", total_data_point, "data points"
         
-		if totalDataPoints == 0:
+		if total_data_point == 0:
 			empty_bins = empty_bins + 1
 			print "No data point is in Bin No.", index1+1, ". Average calculation is not necessary"
 		else:
 			# calculate the average of MCAs
 			# print "Calculating Average of MCA1."
-			mca1AvgArray[index1] = mca1AvgArray[index1] / totalDataPoints
+			mca1_bin_array[index1] = mca1_bin_array[index1] / total_data_point
 			# print "Calculation Average of MCA1 is completed."
 			# print "Calculating Average of MCA2."
-			mca2AvgArray[index1] = mca2AvgArray[index1] / totalDataPoints
+			mca2_bin_array[index1] = mca2_bin_array[index1] / total_data_point
 			# print "Calculation Average of MCA2 is completed."
 			# print "Calculating Average of MCA3."
-			mca3AvgArray[index1] = mca3AvgArray[index1] / totalDataPoints
+			mca3_bin_array[index1] = mca3_bin_array[index1] / total_data_point
 			# print "Calculation Average of MCA3 is completed."
 			# print "Calculating Average of MCA4."
-			mca4AvgArray[index1] = mca4AvgArray[index1] / totalDataPoints
+			mca4_bin_array[index1] = mca4_bin_array[index1] / total_data_point
 			# print "Calculation Average of MCA4 is completed."
 			# print
 
 	print "Calculation completed."
 	print
-	return mca1AvgArray[:-empty_bins], mca2AvgArray[:-empty_bins], mca3AvgArray[:-empty_bins], mca4AvgArray[:-empty_bins], empty_bins
+	return mca1_bin_array[:-empty_bins], mca2_bin_array[:-empty_bins], mca3_bin_array[:-empty_bins], mca4_bin_array[:-empty_bins], empty_bins
 
 
-def calculate_avg_scalers(arrayOfBins, arrayOfPoints):
+def calculate_bin_scalers(arrayOfBins, scaler_array):
 
-	binNum = len(arrayOfBins)
+	bin_num = len(arrayOfBins)
 	empty_bins = 0
-	teyAvgArray = np.zeros(binNum)
-	i0AvgArray = np.zeros(binNum)
-	diodeAvgArray = np.zeros(binNum)
+	tey_bin_array = np.zeros(bin_num)
+	i0_bin_array = np.zeros(bin_num)
+	diode_bin_array = np.zeros(bin_num)
     
 	print "Start calcualting Average of I0, TEY & Diode..."
     
-	for index1 in range (0, binNum):
+	for index1 in range (0, bin_num):
 		for index2 in range (0, len(arrayOfBins[index1])):
 			# get index of scans
-			indexOfScan = arrayOfBins[index1][index2][0]
-			# print indexOfScan
+			index_of_scan = arrayOfBins[index1][index2][0]
+			# print index_of_scan
 			# get index of data points
-			indexOfDataPoint = arrayOfBins[index1][index2][1]
-			# print indexOfDataPoint_2
+			index_of_data_point = arrayOfBins[index1][index2][1]
+			# print index_of_data_point_2
             
 			# calculate the sum of data (TEY, I0, Diode)
-			teyAvgArray[index1] = teyAvgArray[index1] + arrayOfPoints[indexOfScan][0]['TEY'][indexOfDataPoint]
-			i0AvgArray[index1] = i0AvgArray[index1] + arrayOfPoints[indexOfScan][0]['I0'][indexOfDataPoint]
-			diodeAvgArray[index1] = diodeAvgArray[index1] + arrayOfPoints[indexOfScan][0]['Diode'][indexOfDataPoint]
+			tey_bin_array[index1] = tey_bin_array[index1] + scaler_array[index_of_scan][0][index_of_data_point]
+			i0_bin_array[index1] = i0_bin_array[index1] + scaler_array[index_of_scan][1][index_of_data_point]
+			diode_bin_array[index1] = diode_bin_array[index1] + scaler_array[index_of_scan][2][index_of_data_point]
 
 		# get the total number of data points in a particular bins
-		totalDataPoints = len(arrayOfBins[index1])
-		# print "Bin No.", index1+1, "; it contains ", totalDataPoints, "data point"   
+		total_data_point = len(arrayOfBins[index1])
+		# print "Bin No.", index1+1, "; it contains ", total_data_point, "data point"   
         
-		if totalDataPoints == 0:
+		if total_data_point == 0:
 			empty_bins = empty_bins + 1
 			print "No data point is in Bin No.", index1+1, ". Average calculation is not necessary"
 		else:
-			# print totalDataPoints
+			# print total_data_point
 			# calculate the average of data (TEY, I0, Diode)
 			# print "Calculating Average of TEY."
-			teyAvgArray[index1] = teyAvgArray[index1] / totalDataPoints
+			tey_bin_array[index1] = tey_bin_array[index1] / total_data_point
 			# print "Calculation Average of TEY is completed."
 			# print "Calculating Average of I0."
-			i0AvgArray[index1] = i0AvgArray[index1] / totalDataPoints
+			i0_bin_array[index1] = i0_bin_array[index1] / total_data_point
 			# print "Calculation Average of I0 is completed."
 			# print "Calculating Average of Diode."
-			diodeAvgArray[index1] = diodeAvgArray[index1] / totalDataPoints
+			diode_bin_array[index1] = diode_bin_array[index1] / total_data_point
 			# print "Calculation Average of Diode is completed."
-			# print "Index of bins:", index1, "   Average of TEY:", TEYAvgArray[index1]
-			# print "Index of bins:", index1, "   Average of I0:", I0AvgArray[index1]
-			# print "Index of bins:", index1, "   Average of Diode:", DiodeAvgAverage[index1]
+			# print "Index of bins:", index1, "   Average of TEY:", tey_bin_array[index1]
+			# print "Index of bins:", index1, "   Average of I0:", i0_bin_array[index1]
+			# print "Index of bins:", index1, "   Average of Diode:", diode_bin_array[index1]
 			# print
 
 	print "Calculation completed."
 	print
     
-	return teyAvgArray[:-empty_bins], i0AvgArray[:-empty_bins], diodeAvgArray[:-empty_bins]
+	return tey_bin_array[:-empty_bins], i0_bin_array[:-empty_bins], diode_bin_array[:-empty_bins]
 
 
-def plot_excitation_emission_matrix(bins_mean_array, avg_mca, name):
+def plot_excitation_emission_matrix(bins_mean_array, bin_mca, name):
     
 	print "Plotting incident v emission energy coordinate based on average of SDD(MCA)"    
 	plt.close('all')
 	# MCA is SDD and SDD is MCA
 	mca_dict = {'SDD1': 0, 'SDD2': 1, 'SDD3': 2, 'SDD4': 3}
 	sub_mca_array_index = mca_dict[name]
-	sub_mca_array = avg_mca[sub_mca_array_index]
+	sub_mca_array = bin_mca[sub_mca_array_index]
     
 	num_of_bin = len (bins_mean_array)
 	num_of_emission_bins = len(sub_mca_array[0])
@@ -243,7 +241,7 @@ def plot_excitation_emission_matrix(bins_mean_array, avg_mca, name):
 	print "Emission Energy range:", bin_num_for_y[0], "-", bin_num_for_y[-1]
 	return bin_num_for_y
     
-def get_pfy_avg(mca_avg_array, start_energy, stop_energy):
+def get_pfy_bin(mca_bin_array, start_energy, stop_energy):
 
 	print "Getting PFY ROIs"
 
@@ -252,38 +250,38 @@ def get_pfy_avg(mca_avg_array, start_energy, stop_energy):
 	pfy3=[]
 	pfy4=[]
 
-	for i in range(0, len(mca_avg_array[0])):
-		pfy1.append(np.sum(mca_avg_array[0][i][start_energy:stop_energy]))
-		pfy2.append(np.sum(mca_avg_array[1][i][start_energy:stop_energy]))
-		pfy3.append(np.sum(mca_avg_array[2][i][start_energy:stop_energy]))
-		pfy4.append(np.sum(mca_avg_array[3][i][start_energy:stop_energy]))
+	for i in range(0, len(mca_bin_array[0])):
+		pfy1.append(np.sum(mca_bin_array[0][i][start_energy:stop_energy]))
+		pfy2.append(np.sum(mca_bin_array[1][i][start_energy:stop_energy]))
+		pfy3.append(np.sum(mca_bin_array[2][i][start_energy:stop_energy]))
+		pfy4.append(np.sum(mca_bin_array[3][i][start_energy:stop_energy]))
 	
 	return pfy1, pfy2, pfy3, pfy4
 
 
-def get_one_pfy_avg(mca_avg_array, start_energy, stop_energy):
+def get_one_pfy_bin(mca_bin_array, start_energy, stop_energy):
 
 	print "Getting PFY ROIs"
 
 	pfy=[]
 
-	for i in range(0, len(mca_avg_array)):
-		pfy.append(np.sum(mca_avg_array[i][start_energy:stop_energy]))
+	for i in range(0, len(mca_bin_array)):
+		pfy.append(np.sum(mca_bin_array[i][start_energy:stop_energy]))
 	return pfy
 
 
 # plot a kind of average scaler
-def plot_avg_xas(mean_energy_array, name, scaler_data=None, pfy_data=None):
+def plot_bin_xas(mean_energy_array, name, scaler_data=None, pfy_data=None):
 	plt.close('all')
 	if name == "TEY" or name == "I0" or name == "Diode":
-		plot_avg_xas_scaler(mean_energy_array, scaler_data, name)
+		plot_bin_xas_scaler(mean_energy_array, scaler_data, name)
 	elif name == "PFY_SDD1" or name == "PFY_SDD2" or name == "PFY_SDD3" or name == "PFY_SDD4":
-		plot_avg_xas_pfy(mean_energy_array, pfy_data, name)
+		plot_bin_xas_pfy(mean_energy_array, pfy_data, name)
 	else:
 		print "Errors with the name input"
 
         
-def plot_avg_xas_scaler(mean_energy_array, scaler_data, name):
+def plot_bin_xas_scaler(mean_energy_array, scaler_data, name):
 	plt.close('all')
 	scaler_dict = {'TEY': 0, 'I0': 1, 'Diode':2}
 	scaler_index = scaler_dict[name]
@@ -292,7 +290,7 @@ def plot_avg_xas_scaler(mean_energy_array, scaler_data, name):
 	plt.ylabel(name)
 	plt.show()
     
-def plot_avg_xas_pfy(mean_energy_array, pfy_data, name):
+def plot_bin_xas_pfy(mean_energy_array, pfy_data, name):
 	plt.close('all')
     
 	pfy_dict = {'PFY_SDD1': 0, 'PFY_SDD2': 1, 'PFY_SDD3': 2, 'PFY_SDD4': 3}
@@ -303,7 +301,7 @@ def plot_avg_xas_pfy(mean_energy_array, pfy_data, name):
 	plt.show()
 
 
-def plot_avg_xas_all(energy_array, scaler_array, pfy_data):
+def plot_bin_xas_all(energy_array, scaler_array, pfy_data):
 	
 	print "Plotting average XAS."    
 	plt.close('all')
---
+++ scripts/open_hdf5.py
@@ -0,0 +1,38 @@
+import numpy as np
+import h5py
+
+def read_hdf5(file_directory):
+	energy_array = []
+	scaler_array = []
+	mca_array = []
+	with h5py.File(file_directory,'r') as hf:
+		print('List of arrays in this file: \n', hf.keys())
+		total_num = len(hf.keys())
+		scaler_array = [[[],[],[]] for i in range(total_num)]
+		for i in range (0, total_num):
+			scan = mca_data = hf.get(hf.keys()[i])
+			energy_array.append(scan['data']['Energy'][0:])
+			scaler_array[i][0] = scan['data']['TEY'][0:]
+			scaler_array[i][1] = scan['data']['I0'][0:]
+			scaler_array[i][2] = scan['data']['Diode'][0:]
+			#mca_data = scan['data']['_mca_']
+			#mca_array.append( get_mca_from_hdf5(mca_data) )
+	return energy_array, mca_array, scaler_array
+
+
+def get_mca_from_hdf5(mca_data):
+	array_mca1 = []
+	array_mca2 = []
+	array_mca3 = []
+	array_mca4 = []
+	for i in range (0, len(mca_data)):
+		if mca_data[i][0] == 1.0:
+			# get 256 values and exclude the first column in the dataset
+			array_mca1.append(mca_data[i][1:])
+		elif mca_data[i][0] == 2.0:
+			array_mca2.append(mca_data[i][1:])
+		elif mca_data[i][0] == 3.0:
+			array_mca3.append(mca_data[i][1:])
+		else:
+			array_mca4.append(mca_data[i][1:])
+	return array_mca1, array_mca2, array_mca3, array_mca4
