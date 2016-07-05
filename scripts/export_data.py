be4b19b95a1dfe61d2f050e415fa6707d774330d
Rongli H <roh919@mail.usask.ca>
2016-07-05 15:21:46 -0600

    Updated export data functions
    Also, updated hdf5  normalization examples

--- scripts/export_data.py
+++ scripts/export_data.py
@@ -1,78 +1,126 @@
 # Open date file and get scan details
 
 from numpy import arange
-#from average_plot import *
-
+from open_spec import *
+import h5py
+import numpy as np
 
 def export_data(export_file_directory, origin_file_directory, mean_energy_array, name, avg_scaler=None, pfy_data=None):
-	headers = get_header(origin_file_directory)
+
 	# MCA is SDD; after getting PFY of ROI then it becomes PFY_SDD
 	pfy_dict = {'PFY_SDD1': 0, 'PFY_SDD2': 1, 'PFY_SDD3': 2, 'PFY_SDD4': 3}
 	scaler_dict = {'TEY': 0, 'I0': 1, 'Diode': 2}
     
 	if name == "PFY_SDD1" or name == "PFY_SDD2" or name == "PFY_SDD3" or name == "PFY_SDD4":
 		sub_pfy_index = int(pfy_dict[name])
-		export_pfy(export_file_directory, headers, mean_energy_array, pfy_data[sub_pfy_index], name)
+		export_pfy(export_file_directory, origin_file_directory, mean_energy_array, pfy_data[sub_pfy_index], name)
 		print "Export data complete!"
 
 	elif name == "TEY" or name == "I0" or name == "Diode":
 		sub_scaler_index = scaler_dict[name]
-		export_scaler(export_file_directory, headers, mean_energy_array, avg_scaler[sub_scaler_index], name)
+		export_scaler(export_file_directory, origin_file_directory, mean_energy_array, avg_scaler[sub_scaler_index], name)
 		print "Export data complete!"
 
 	else:
 		print "Unable to export data."
 
 
-def get_header(file_directory):
-	with open(file_directory) as content:
-		lines = content.readlines()
-	return lines[:19]
-
-
-def get_date_time(header_lines):
-	str_date_time = header_lines[2]
-	# print str_date_time[3:]
+def get_date_time(opened_file):
+	str_date_time = opened_file['1'].attrs['file_date']
 	return str_date_time[3:]
 
 
-def get_original_file_name(header_lines):
-	str_file_name = header_lines[0]
+def get_original_file_name(opened_file):
+	str_file_name = opened_file['1'].attrs['file_origin']
 	return str_file_name[3:]
 
 
-def get_grating(header_lines):
-	str_line_17 = header_lines[17]
-	str_list = str_line_17.split()
-	output = str_list[-2]+" "+str_list[-1]
-	return output
+def get_comments(file_directory):
+	with open(file_directory) as content:
+		lines = content.readlines()
+		temp = lines[:40]
+		comments = []
+		for i in range (0, 40):
+			if temp[i][:2] == '#C':
+				comments.append(temp[i])
+	return comments
 
-def get_exit_slit(header_lines):
-	str_line_18 = header_lines[18]
-	str_list = str_line_18.split()
-	output = str_list[8].rstrip(',')
-	return output
 
-def get_stripe(header_lines):
-	str_line_18 = header_lines[18]
-	str_list = str_line_18.split()
-	output = str_list[-1].rstrip('.')
-	return output
+def get_comment_details(comments):
+	split_comma = []
+	split_dot = []
+	for i in range(0, len(comments)):
+		split_comma.append(comments[i].split(','))      
+	for i in range(0, len(split_comma)):
+		for j in range(0, len(split_comma[i])):
+			split_dot.append(split_comma[i][j].split(':'))
+            
+	for i in range (0, len(split_dot)):
+		if split_dot[i][0] == ' Grating':
+			grating = split_dot[i][1].strip()
+			exit_slit = split_dot[i+1][-1].strip()
+			stripe = split_dot[i+2][-1].strip()
+	return grating[:-1], exit_slit, stripe[:-1]
+
+
+def get_header_hdf5(file_directory):
+	with h5py.File(file_directory,'r') as hf:
+		comments = hf.get('S1/comments')
+		comments = np.array(comments)
+		date = hf.get('S1/date')
+		date = np.array(date)[0]
+	return comments, date
+
+def get_grating_hdf5(comments):
+	parsed_str = comments[0].split('\n')
+	grating_str = [x.strip() for x in parsed_str[1].split(",")]
+	print grating_str[-1]
+	return grating_str[-1][9:-1]    
+
+
+def get_exit_slit_and_stripte(comments):
+	parsed_str = comments[0].split('\n')
+	temp_str = [x.strip() for x in parsed_str[2].split(",")]
+	print temp_str[-2]
+	temp_str_2 = [x.strip() for x in temp_str[-2].split(":")]
+	exit_slit_str = temp_str_2[-1]    
+	stripe_str = temp_str[-1][7:-1]
     
-def export_pfy(export_file_directory, headers, mean_energy_array, sub_pfy, name):
+	return exit_slit_str, stripe_str
+
+def check_file_type(origin_file_directory):
+	file_directory = origin_file_directory.split('.')
+	file_extension = file_directory[-1]
+	file_name = file_directory[0].split('/')[-1]
+	return file_extension, file_name
+
+    
+def export_pfy(export_file_directory, origin_file_directory, mean_energy_array, sub_pfy, name):
+	file_extension, original_file_name = check_file_type(origin_file_directory)
+	if file_extension == "dat":
+		opened_file = open_spec_data_file(origin_file_directory)
+		date = get_date_time(opened_file)
+		comments = get_comments(origin_file_directory)
+		grating, exit_slit, stripe = get_comment_details(comments)
+	else:
+		comments, date = get_header_hdf5(origin_file_directory)
+		grating = get_grating_hdf5(comments)
+		exit_slit, stripe = get_exit_slit_and_stripte(comments)
+		# date = date[0]
+
 	with open(export_file_directory, "w") as out_file:
 		# write header into the data file
-		out_file.write("# Beamline.file-content: binned and averaged" + name + "\n")
-		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
+		out_file.write("# Beamline.file-content: binned and averaged " + name + "\n")
+		str_origin_file_name = "# Beamline.origin-filename: " + original_file_name + "\n"
 		out_file.write(str_origin_file_name)
 		out_file.write("# Beamline.name: SGM\n")
-		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
+		str_grating = "# Beamline.grating: " + grating + "\n"
 		out_file.write(str_grating)
-		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
+		str_stripe = "# Beamline.stripe: " + stripe + "\n"
 		out_file.write(str_stripe)
-		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
+		str_exit_slit = "# Beamline.exit-slit: " + exit_slit + "\n"
 		out_file.write(str_exit_slit)
-		str_date_time = "# Time.start: " + get_date_time(headers)
+		str_date_time = "# Time.start: " + date + "\n"
 		out_file.write(str_date_time)
 		out_file.write("#-----------------------------------------------------------\n")
     
@@ -93,20 +141,32 @@ def export_pfy(export_file_directory, headers, mean_energy_array, sub_pfy, name)
 			out_file.write(out_string)
 
 
-def export_scaler(export_file_directory, headers, mean_energy_array, sub_avg_scaler, name):
+def export_scaler(export_file_directory, origin_file_directory, mean_energy_array, sub_avg_scaler, name):
+	file_extension, original_file_name = check_file_type(origin_file_directory)
+	if file_extension == "dat":
+		opened_file = open_spec_data_file(origin_file_directory)
+		date = get_date_time(opened_file)
+		comments = get_comments(origin_file_directory)
+		grating, exit_slit, stripe = get_comment_details(comments)
+	else:
+		comments, date = get_header_hdf5(origin_file_directory)
+		grating = get_grating_hdf5(comments)
+		exit_slit, stripe = get_exit_slit_and_stripte(comments)
+		# date = date[0]
+
 	with open(export_file_directory, "w") as out_file:
 		# write header into the data file
-		out_file.write("# Beamline.file-content: binned and averaged" + name + "\n")
-		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
+		out_file.write("# Beamline.file-content: binned and averaged " + name + "\n")
+		str_origin_file_name = "# Beamline.origin-filename: " + original_file_name + "\n"
 		out_file.write(str_origin_file_name)
 		out_file.write("# Beamline.name: SGM\n")
-		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
+		str_grating = "# Beamline.grating: " + grating + "\n"
 		out_file.write(str_grating)
-		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
+		str_stripe = "# Beamline.stripe: " + stripe + "\n"
 		out_file.write(str_stripe)
-		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
+		str_exit_slit = "# Beamline.exit-slit: " + exit_slit + "\n"
 		out_file.write(str_exit_slit)
-		str_date_time = "# Time.start: " + get_date_time(headers)
+		str_date_time = "# Time.start: " + date + "\n"
 		out_file.write(str_date_time)
 		out_file.write("#-----------------------------------------------------------\n")
     
@@ -127,64 +187,75 @@ def export_scaler(export_file_directory, headers, mean_energy_array, sub_avg_sca
 			out_file.write(out_string)
 
 
-def export_eem(export_file_directory, origin_file_directory, mean_energy_array, emission_energy, color_variable, name):
-	headers = get_header(origin_file_directory)
-	with open(export_file_directory, "w") as out_file:
-		out_file.write("# Beamline.file-content: " + name + " data from Excitation Emission Matrix\n")
-		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
-		out_file.write(str_origin_file_name)
-		out_file.write("# Beamline.name: SGM\n")
-		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
-		out_file.write(str_grating)
-		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
-		out_file.write(str_stripe)
-		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
-		out_file.write(str_exit_slit)
-		str_date_time = "# Time.start: " + get_date_time(headers)
-		out_file.write(str_date_time)
-		out_file.write("#-----------------------------------------------------------\n")
-
-		# write table header into the data file
-		out_file.write("# Incident_Energy\t")
-		out_file.write("Emission_Energy\t")
-		out_file.write(name)
-		out_file.write("\n")
-
-		for i in range(0, len(mean_energy_array)):
-			for j in range(0, len(emission_energy)):
-				out_string = ""
-				# print mean_energy_array[i]
-				out_string += str(mean_energy_array[i])
-				out_string += "\t"
-				out_string += str(emission_energy[j])
-				out_string += "\t"
-				out_string += str(color_variable[i][j])
-				# print sub_avg_scaler[i]
-				out_string += "\n"
-				# print out_string
-				out_file.write(out_string)
-	print ("Export data complete.")
+#def export_eem(export_file_directory, origin_file_directory, mean_energy_array, emission_energy, color_variable, name):
+#	headers = get_header(origin_file_directory)
+#	with open(export_file_directory, "w") as out_file:
+#		out_file.write("# Beamline.file-content: " + name + " data from Excitation Emission Matrix\n")
+#		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
+#		out_file.write(str_origin_file_name)
+#		out_file.write("# Beamline.name: SGM\n")
+#		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
+#		out_file.write(str_grating)
+#		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
+#		out_file.write(str_stripe)
+#		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
+#		out_file.write(str_exit_slit)
+#		str_date_time = "# Time.start: " + get_date_time(headers)
+#		out_file.write(str_date_time)
+#		out_file.write("#-----------------------------------------------------------\n")
+#
+#		# write table header into the data file
+#		out_file.write("# Incident_Energy\t")
+#		out_file.write("Emission_Energy\t")
+#		out_file.write(name)
+#		out_file.write("\n")
+#
+#		for i in range(0, len(mean_energy_array)):
+#			for j in range(0, len(emission_energy)):
+#				out_string = ""
+#				# print mean_energy_array[i]
+#				out_string += str(mean_energy_array[i])
+#				out_string += "\t"
+#				out_string += str(emission_energy[j])
+#				out_string += "\t"
+#				out_string += str(color_variable[i][j])
+#				# print sub_avg_scaler[i]
+#				out_string += "\n"
+#				# print out_string
+#				out_file.write(out_string)
+#	print ("Export data complete.")
                 
                 
 def export_all (export_file_directory, origin_file_directory, mean_energy_array, avg_scaler, avg_pfy):
-	headers = get_header(origin_file_directory)
+	file_extension, original_file_name = check_file_type(origin_file_directory)
+	if file_extension == "dat":
+		opened_file = open_spec_data_file(origin_file_directory)
+		date = get_date_time(opened_file)
+		comments = get_comments(origin_file_directory)
+		grating, exit_slit, stripe = get_comment_details(comments)
+	else:
+		comments, date = get_header_hdf5(origin_file_directory)
+		grating = get_grating_hdf5(comments)
+		exit_slit, stripe = get_exit_slit_and_stripte(comments)
+
 	with open(export_file_directory, "w") as out_file:
+		# write header into the data file
 		out_file.write("# Beamline.file-content: all data\n")
-		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
+		str_origin_file_name = "# Beamline.origin-filename: " + original_file_name + "\n"
 		out_file.write(str_origin_file_name)
 		out_file.write("# Beamline.name: SGM\n")
-		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
+		str_grating = "# Beamline.grating: " + grating + "\n"
 		out_file.write(str_grating)
-		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
+		str_stripe = "# Beamline.stripe: " + stripe + "\n"
 		out_file.write(str_stripe)
-		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
+		str_exit_slit = "# Beamline.exit-slit: " + exit_slit + "\n"
 		out_file.write(str_exit_slit)
-		str_date_time = "# Time.start: " + get_date_time(headers)
+		str_date_time = "# Time.start: " + date + "\n"
 		out_file.write(str_date_time)
 		out_file.write("#-----------------------------------------------------------\n")
-		# write table header into the data file
-		out_file.write("# Energy\tTEY\tI0\tDiode\tPFY_SDD1\tPFY_SDD2\tPFY_SDD3\tPFY_SDD4\n")
 
+        # write table header into the data file
+		out_file.write("# Energy\tTEY\tI0\tDiode\tPFY_SDD1\tPFY_SDD2\tPFY_SDD3\tPFY_SDD4\n")
 		for i in range(0, len(mean_energy_array)):
 			out_string = ""
 			# print mean_energy_array[i]
@@ -211,20 +282,30 @@ def export_all (export_file_directory, origin_file_directory, mean_energy_array,
             
         
 def export_normalized_data(export_file_directory, origin_file_directory, column1, column2, column1_name, column2_name):
-	headers = get_header(origin_file_directory)
+	file_extension, original_file_name = check_file_type(origin_file_directory)
+	if file_extension == "dat":
+		opened_file = open_spec_data_file(origin_file_directory)
+		date = get_date_time(opened_file)
+		comments = get_comments(origin_file_directory)
+		grating, exit_slit, stripe = get_comment_details(comments)
+	else:
+		comments, date = get_header_hdf5(origin_file_directory)
+		grating = get_grating_hdf5(comments)
+		exit_slit, stripe = get_exit_slit_and_stripte(comments)
+
 	with open(export_file_directory, "w") as out_file:
-		str_file_content = "# Beamline.file-content: normalized" + column2_name + "\n"
-		out_file.write(str_file_content)
-		str_origin_file_name = "# Beamline.origin-filename: " + get_original_file_name(headers)
+		# write header into the data file
+		out_file.write("# Beamline.file-content: Normalized " + column2_name + "\n")
+		str_origin_file_name = "# Beamline.origin-filename: " + original_file_name + "\n"
 		out_file.write(str_origin_file_name)
 		out_file.write("# Beamline.name: SGM\n")
-		str_grating = "# Beamline.grating: " + get_grating(headers) + "\n"
+		str_grating = "# Beamline.grating: " + grating + "\n"
 		out_file.write(str_grating)
-		str_stripe = "# Beamline.stripe: " + get_stripe(headers) + "\n"
+		str_stripe = "# Beamline.stripe: " + stripe + "\n"
 		out_file.write(str_stripe)
-		str_exit_slit = "# Beamline.exit-slit: " + get_exit_slit(headers) + "\n"
+		str_exit_slit = "# Beamline.exit-slit: " + exit_slit + "\n"
 		out_file.write(str_exit_slit)
-		str_date_time = "# Time.start: " + get_date_time(headers)
+		str_date_time = "# Time.start: " + date + "\n"
 		out_file.write(str_date_time)
 		out_file.write("#-----------------------------------------------------------\n")
 		# write table header into the data file
@@ -241,4 +322,4 @@ def export_normalized_data(export_file_directory, origin_file_directory, column1
 			# print out_string
 			out_file.write(out_string)
 	print ("Export data complete.")
-    
+
